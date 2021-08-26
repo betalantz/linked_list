fe21cca56fe76fbc831ae27279f3d35eec610d93
betalantz <betalantz@gmail.com>
2021-08-26 13:04:12 -0700

    initial commit

---
+++ singly_linked_list.rb
@@ -0,0 +1,36 @@
+class Node
+
+    attr_accessor :next, :value
+
+    def initialize(value)
+        @value, @next = value, nil
+    end
+end
+
+class LinkedList
+
+    def initialize(head = nil)
+        @head = head
+    end
+
+    def add
+
+    end
+
+    def insert_at(index)
+
+    end
+
+    def remove_at(index)
+
+    end
+
+    def node_at(index)
+        
+    end
+
+    def print
+
+    end
+
+end
