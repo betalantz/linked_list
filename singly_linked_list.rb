f87cb8d8f1446b30533e1a60b1a76f80df975854
betalantz <betalantz@gmail.com>
2021-08-31 15:09:10 -0700

    adds add add and print to solution branch

--- singly_linked_list.rb
+++ singly_linked_list.rb
@@ -13,8 +13,18 @@ class LinkedList
         @head = head
     end
 
-    def add
-
+    def add(value)
+        newNode = Node.new(value)
+        if !@head
+            @head = newNode
+        else
+            current = @head
+            while current.next
+                current = current.next
+            end
+            current.next = newNode
+        end
+        
     end
 
     def insert_at(index)
@@ -26,11 +36,22 @@ class LinkedList
     end
 
     def node_at(index)
-        
+
     end
 
     def print
-
+        current = @head
+        while current
+            puts current.value
+            current = current.next
+        end
     end
 
 end
+
+list = LinkedList.new
+list.add(2)
+list.print
+list.add(4)
+list.add(6)
+list.print
